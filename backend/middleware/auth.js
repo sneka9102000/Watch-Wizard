0b4549f65e446185d768219b3ce1b87824cbdabc
sneka9102000 <snekakumar925@gmail.com>
2022-07-07 16:34:57 +0530

    commit

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -7,13 +7,11 @@ const User = require ("../models/userModel")
 
 exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
     const { token } = req.cookies;
-    // console.log(req.cookies)
-    //console.log("token",token);
+
 
 if (!token) {
     return next(new ErrorHandler("Please Login to access this resource", 401));
   }
-//  console.log("process token : ",process.env.JWT_SECRET)
   const decodedData = jwt.verify(token, process.env.JWT_SECRET);
 
   req.user = await User.findById(decodedData.id);
